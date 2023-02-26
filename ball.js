b9c3e9a739550b4ca7da9fc61707b0278af75b47
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-02-26 12:59:56 -0800

    added more things to ball

--- ball.js
+++ ball.js
@@ -1,7 +1,11 @@
+const INITIAL_VELOCITY = .025
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
-        /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball    */    
+        /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball */
+        this.reset()
+
     }
 
     get x() {
@@ -10,6 +14,7 @@ export default class Ball {
 
     set x(value) {
         this.ballElem.style.setProperty("--x", value)
+        /*value gets passed in through x */
     }
 
     get y() {
@@ -18,10 +23,29 @@ export default class Ball {
 
     set y(value) {
         this.ballElem.style.setProperty("--y", value)
+        /*value gets passed in through y */
+    }
+
+    reset() {
+        /* reset the properties*/
+        this.x = 50
+        this.y = 50
+        this.direction = { x: 0 }
+        while (Math.abs(this.direction.x) >= .2 || Math.abs(this.direction.x) >= .9) {
+            const heading = randomNumberBetween(0, 2 * MathPI)
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+        }
+        this.velocity = INITIAL_VELOCITY
+
     }
 
     update(delta) {
         this.x = 5
+        /* sets position of the ball to 5*/
         this.y = 15
     }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min
 }
