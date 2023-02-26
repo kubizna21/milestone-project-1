51de6f57b3fb179f3560402da4ec751c846a4265
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-02-26 12:28:35 -0800

    added ball

--- ball.js
+++ ball.js
@@ -1,6 +1,27 @@
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
-        /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball    */
+        /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball    */    
+    }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value)
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value)
+    }
+
+    update(delta) {
+        this.x = 5
+        this.y = 15
     }
 }
