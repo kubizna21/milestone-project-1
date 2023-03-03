170d57a000a5b8fb7ffdf8e64d5881875f689f25
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-03-02 20:13:08 -0800

    created paddle.js and added things too script.js

--- ball.js
+++ ball.js
@@ -1,51 +1,86 @@
-const INITIAL_VELOCITY = .025
+const INITIAL_VELOCITY = 0.025
+/* making it a constant variable*/
+const VELOCITY_INCREASE = 0.00001
+/*increase the balls speed */
 
 export default class Ball {
-    constructor(ballElem) {
-        this.ballElem = ballElem
-        /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball */
-        this.reset()
+  constructor(ballElem) {
+    this.ballElem = ballElem
+    /*corresponds to the ball, ballElem passed in ball class to use and interact with the ball */
 
+    this.reset()
+  }
+
+  get x() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+  }
+
+  set x(value) {
+    this.ballElem.style.setProperty("--x", value)
+    /*value gets passed in through x */
+  }
+
+  get y() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+  }
+
+  set y(value) {
+    this.ballElem.style.setProperty("--y", value)
+    /*value gets passed in through y */
+  }
+
+  rect() {
+    return this.ballElem.getBoundingClientRect()
+    /*bounces off the top and bottom of the screen */
+  }
+
+  reset() {
+      /* reset the properties*/
+    this.x = 50
+    this.y = 50
+    /*creating a value for x and y, */
+    this.direction = { x: 0 }
+    while (
+      Math.abs(this.direction.x) <= 0.2 ||
+      Math.abs(this.direction.x) >= 0.9
+      /* moves ball far enough left or right, absolute vaule  */
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI)
+      /*heading in the rotation, PI = 360degress in radians,   */
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+      /*with radians I am able to use cosine and sine to determine the x and y direction */
+    }
+    this.velocity = INITIAL_VELOCITY
+  }
+
+  update(delta, paddleRects) {
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    this.velocity += VELOCITY_INCREASE * delta
+    /*the ball will increase speed */
+    const rect = this.rect()
+
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1
+      /*when the ball goes hits the top of the page it bounces down and when the ball hits the bottom of the page it bounces up */
     }
 
-    get x() {
-        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    if (paddleRects.some(r => isCollision(r, rect))) {
+      this.direction.x *= -1
     }
-
-    set x(value) {
-        this.ballElem.style.setProperty("--x", value)
-        /*value gets passed in through x */
-    }
-
-    get y() {
-        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
-    }
-
-    set y(value) {
-        this.ballElem.style.setProperty("--y", value)
-        /*value gets passed in through y */
-    }
-
-    reset() {
-        /* reset the properties*/
-        this.x = 50
-        this.y = 50
-        this.direction = { x: 0 }
-        while (Math.abs(this.direction.x) >= .2 || Math.abs(this.direction.x) >= .9) {
-            const heading = randomNumberBetween(0, 2 * MathPI)
-            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
-        }
-        this.velocity = INITIAL_VELOCITY
-
-    }
-
-    update(delta) {
-        this.x = 5
-        /* sets position of the ball to 5*/
-        this.y = 15
-    }
+  }
 }
 
 function randomNumberBetween(min, max) {
-    return Math.random() * (max - min) + min
+  return Math.random() * (max - min) + min
+/*random number between 0 and 1, scale the vaule in the range, and the min is lowest number it can get   */
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  )
 }
--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 import Ball from "./Ball.js"
+import Paddle from "./Paddle.js"
 
 const ball = new Ball(document.getElementById("ball"))
 /* selecting ball html element, creating new class for ball, now can use it here  */
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
 
 let lastTime
 function update(time) {
     /* how much time has passed from a previous frame to a new frame*/
-    if (lastTime !=null) {
-        const delta = time - lastTime
-        /*update code if we have a lastTime, if no lastTime nothing to compare too */
-        ball.update(delta)
-        /* update and passing it into the delta, all movesments in the game are based on the delta */
+  if (lastTime != null) {
+    const delta = time - lastTime
+     /*update code if we have a lastTime, if no lastTime nothing to compare too */
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+   /* update and passing it into the delta, all movesments in the game are based on the delta */
+    computerPaddle.update(delta, ball.y)
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    )
 
-    }
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
 
-    lastTime = time
-    window.requestAnimationFrame(update)
+    if (isLose()) handleLose()
+  }
 
+  lastTime = time
+  window.requestAnimationFrame(update)
 }
+
+function isLose() {
+  const rect = ball.rect()
+  return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose() {
+  const rect = ball.rect()
+  if (rect.right >= window.innerWidth) {
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+  } else {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+  }
+  ball.reset()
+  computerPaddle.reset()
+}
+
+document.addEventListener("mousemove", e => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100
+  /*e/y = vaule between 0 and 1. vaule between 0 and 100 */
+})
 
 window.requestAnimationFrame(update)
 /* update loop, everytime it changes on screen it will be called  */
-
